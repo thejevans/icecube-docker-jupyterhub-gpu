fc26124391f49e0de972bf058fbf0f9b1e45c78e
thejevans <thejevans@gmail.com>
2021-11-10 02:52:58 +0000

    initial commit

---
+++ jupyterhub/jupyterhub_config.py
@@ -0,0 +1,43 @@
+import os
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+c.JupyterHub.load_roles = [
+    {
+        "name": "jupyterhub-idle-culler-role",
+        "scopes": [
+            "list:users",
+            "read:users:activity",
+            "read:servers",
+            "delete:servers",
+            # "admin:users", # if using --cull-users
+        ],
+        # assignment of role's permissions to:
+        "services": ["jupyterhub-idle-culler-service"],
+    }
+]
+
+c.JupyterHub.services = [
+    {
+        "name": "jupyterhub-idle-culler-service",
+        "command": [
+            sys.executable,
+            "-m", "jupyterhub_idle_culler",
+            "--timeout=3600",
+        ],
+        # "admin": True,
+    }
+]
+
+# Redirect to JupyterLab, instead of the plain Jupyter notebook
+c.Spawner.default_url = '/lab'
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export $(xargs < .env)
+
+docker volume create --name jupyterhub-data
+
+docker build \
+	-t jupyterhub \
+	--build-arg version=$JUPYTERHUB_VERSION \
+	--build-arg ds_version=$DOCKERSPAWNER_VERSION \
+	./jupyterhub
+
+docker build \
+	-t icecube-jupyter \
+	--build-arg jh_version=$JUPYTERHUB_VERSION \
+	--build-arg jl_version=$JUPYTERLAB_VERSION \
+	./jupyterlab
+
+docker run \
+	--name=jupyterhub_hub \
+	--restart=unless-stopped \
+	-v $DOCKER_SOCKET:/var/run/docker.sock \
+	-v jupyterhub-data:/srv/jupyterhub \
+	-e DOCKER_JUPYTER_IMAGE: icecube-jupyter \
+	-e DOCKER_NETWORK_NAME: ${COMPOSE_PROJECT_NAME}_default \
+	-e HUB_IP: jupyterhub_hub \
+	-p $JUPYTERHUB_PORT:8000 \
+	jupyterhub
